a4dff77674a5aa30c31f01519e846fb2222d4554
LohithHexa <2000160736@hexaware.com>
2025-08-08 17:14:57 +0530

    Update AssetClass.java

--- pms/src/main/java/com/pms/backend/entity/AssetClass.java
+++ pms/src/main/java/com/pms/backend/entity/AssetClass.java
@@ -28,5 +28,62 @@ public class AssetClass {
     @Column(name = "investment_horizon")
     private String investmentHorizon;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getSubClassName() {
+		return subClassName;
+	}
+
+	public void setSubClassName(String subClassName) {
+		this.subClassName = subClassName;
+	}
+
+	public String getSubClassDescription() {
+		return subClassDescription;
+	}
+
+	public void setSubClassDescription(String subClassDescription) {
+		this.subClassDescription = subClassDescription;
+	}
+
+	public String getRisk() {
+		return risk;
+	}
+
+	public void setRisk(String risk) {
+		this.risk = risk;
+	}
+
+	public String getInvestmentHorizon() {
+		return investmentHorizon;
+	}
+
+	public void setInvestmentHorizon(String investmentHorizon) {
+		this.investmentHorizon = investmentHorizon;
+	}
+
     // Getters and setters
+    
 }
